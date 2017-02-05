abdfea2533a2063ff3034c89b30a34a0ea4dec30
SeregaSRG <monohrom98@gmail.com>
2017-02-05 13:34:44 +0300

    Срабатывание событий pause и resume

--- www/app/index.js
+++ www/app/index.js
@@ -32,21 +32,10 @@ var app = {
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        
-
         console.log('Received Event: ' + id);
+        document.addEventListener("pause", onPause, false);
+        document.addEventListener("resume", onResume, false);
     }
 };
-
-
-
-
 
 app.initialize();
